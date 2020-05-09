8f630f25e0be54b821fb5703a1b2c9a609f507f7
sidheartx <siddhart.dny@gmail.com>
2020-05-09 11:37:28 +0530

    Fixed pragma Bug

--- caller/contracts/CallerContract.sol
+++ caller/contracts/CallerContract.sol
@@ -1,4 +1,4 @@
-ragma solidity 0.5.0;
+pragma solidity 0.5.0;
 import "./EthPriceOracleInterface.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 contract CallerContract is Ownable {
